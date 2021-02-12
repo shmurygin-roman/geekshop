fbd6d92abb4a1b64a42526f4f5903a8b8ebbe6c2
Unknown <unknown@example.com>
2021-02-12 07:18:30 -0800

    Lesson 5. Basket counter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+from basketapp.models import Basket
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
@@ -17,6 +18,14 @@ def main(request):
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by("price")
@@ -30,6 +39,7 @@ def products(request, pk=None):
             "category": category,
             "products": products,
             "media_url": settings.MEDIA_URL,
+            "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
@@ -38,6 +48,8 @@ def products(request, pk=None):
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "same_products": same_products,
+        "basket": basket,
     }
     if pk:
         print(f"User select category: {pk}")
