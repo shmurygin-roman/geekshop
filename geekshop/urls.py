c7680cae5469d548cbf088bece008a078c4e8687
Unknown <unknown@example.com>
2021-02-12 07:08:12 -0800

    Lesson 5. Basket app start

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,10 +1,9 @@
+import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
-import mainapp.views as mainapp
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,6 +11,7 @@ urlpatterns = [
     path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
     path("auth/", include("authnapp.urls", namespace="auth")),
+    path("basket/", include("basketapp.urls", namespace="basket")),
 ]
 
 
