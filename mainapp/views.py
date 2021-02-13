0ea8826012ba8f7a1e60b052f22a351bf35f4f63
shmurygin-roman <65447504+shmurygin-roman@users.noreply.github.com>
2021-02-13 00:55:04 -0800

    Lesson 5 (#5)
    * Lesson 5. Code style fix
    
    * Lesson 5. ORM filters
    
    * Lesson 5. Basket app start
    
    * Lesson 5. Basket counter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
+from basketapp.models import Basket
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
 from .models import Contact, Product, ProductCategory
@@ -8,7 +9,7 @@ from .models import Contact, Product, ProductCategory
 def main(request):
     title = "главная"
 
-    products = Product.objects.all()[:5]
+    products = Product.objects.all()[:4]
 
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
@@ -17,12 +18,38 @@ def main(request):
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "all"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by("price")
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+            "basket": basket,
+        }
+        return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "same_products": same_products,
+        "basket": basket,
     }
     if pk:
         print(f"User select category: {pk}")
