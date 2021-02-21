ef1adae9942947f738566192852de1cbbe9cf637
Unknown <unknown@example.com>
2021-02-21 02:47:39 -0800

    lesson_7. Admin link and firewall.

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,14 +1,17 @@
 from authnapp.models import ShopUser
 from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import get_object_or_404, redirect, render
 from mainapp.models import Product, ProductCategory
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_main(request):
     response = redirect("admin:users")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = "админка/пользователи"
     users_list = ShopUser.objects.all().order_by("-is_active", "-is_superuser", "-is_staff", "username")
@@ -16,21 +19,25 @@ def users(request):
     return render(request, "adminapp/users.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
     response = redirect("admin:users")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
     response = redirect("admin:users")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
     response = redirect("admin:users")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def categories(request):
     title = "админка/категории"
     categories_list = ProductCategory.objects.all()
@@ -38,21 +45,25 @@ def categories(request):
     return render(request, "adminapp/categories.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_create(request):
     response = redirect("admin:categories")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_update(request, pk):
     response = redirect("admin:categories")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_delete(request, pk):
     response = redirect("admin:categories")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def products(request, pk):
     title = "админка/продукт"
     category = get_object_or_404(ProductCategory, pk=pk)
@@ -61,21 +72,25 @@ def products(request, pk):
     return render(request, "adminapp/products.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
     response = redirect("admin:categories")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
     response = redirect("admin:categories")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
     response = redirect("admin:categories")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
     response = redirect("admin:categories")
     return response
