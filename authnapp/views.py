d21bb4d6ca3d970e8cdce9a7affca4825d97f382
Unknown <unknown@example.com>
2021-03-23 12:07:08 -0700

    lesson_14. Load testing.

--- authnapp/views.py
+++ authnapp/views.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.db import transaction
 from django.shortcuts import HttpResponseRedirect, render
 from django.urls import reverse
 
-from authnapp.forms import (ShopUserEditForm, ShopUserLoginForm,
-                            ShopUserProfileEditForm, ShopUserRegisterForm)
+from authnapp.forms import ShopUserEditForm, ShopUserLoginForm, ShopUserProfileEditForm, ShopUserRegisterForm
 from authnapp.models import ShopUser
 
 
@@ -56,6 +56,7 @@ def register(request):
     return render(request, "authnapp/register.html", content)
 
 
+@login_required
 @transaction.atomic
 def edit(request):
     title = "редактирование"
