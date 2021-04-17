ecb92ffdb7709c3cd5ed14993755b7c83c1d3ad4
shmurygin-roman <65447504+shmurygin-roman@users.noreply.github.com>
2021-04-17 11:18:36 +0500

    Lesson 16 (#16)
    * lesson_16. QuerySet update.
    
    * lesson_16. F objects.
    
    * lesson_16. F and update
    
    * lesson_16. Make queries.
    
    * lesson_16. Tests.

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.db import connection
+from django.db.models import F
 from django.http import JsonResponse
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 from django.template.loader import render_to_string
@@ -26,9 +28,14 @@ def basket_add(request, pk):
 
     if not basket:
         basket = Basket(user=request.user, product=product)
+        basket.quantity += 1
+    else:
+        basket.quantity = F("quantity") + 1
 
-    basket.quantity += 1
     basket.save()
+
+    update_queries = list(filter(lambda x: "UPDATE" in x["sql"], connection.queries))
+    print(f"query basket_add: {update_queries}")
 
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
@@ -42,24 +49,24 @@ def basket_remove(request, pk):
 
 @login_required
 def basket_edit(request, pk, quantity):
-        try:
-            pk = int(pk)
-            quantity = int(quantity)
-        except Exception as exp:
-            print(f"Wrong input numbers! {exp}")
-            raise exp
-        new_basket_item = Basket.objects.get(pk=int(pk))
+    try:
+        pk = int(pk)
+        quantity = int(quantity)
+    except Exception as exp:
+        print(f"Wrong input numbers! {exp}")
+        raise exp
+    new_basket_item = Basket.objects.get(pk=int(pk))
 
-        if quantity > 0:
-            new_basket_item.quantity = quantity
-            new_basket_item.save()
-        else:
-            new_basket_item.delete()
+    if quantity > 0:
+        new_basket_item.quantity = quantity
+        new_basket_item.save()
+    else:
+        new_basket_item.delete()
 
-        basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
+    basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
 
-        content = {"basket_items": basket_items, "media_url": settings.MEDIA_URL}
+    content = {"basket_items": basket_items, "media_url": settings.MEDIA_URL}
 
-        result = render_to_string("basketapp/includes/inc_basket_list.html", content)
+    result = render_to_string("basketapp/includes/inc_basket_list.html", content)
 
-        return JsonResponse({"result": result})
+    return JsonResponse({"result": result})
